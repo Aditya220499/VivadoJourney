b726b0a9314883d409c6edb227f6d7a6d6cbe893
Aditya Lonkar <adityalonkar99@gmail.com>
2026-02-14 15:37:10 +0530

    creaate environment

---
+++ Adder/TB/environment.sv
@@ -0,0 +1,25 @@
+class environment;
+    generator gen;
+    driver drv;
+    monitor mon;
+    scoreboard scb;
+
+    mailbox #(adder_tx) gen2drv = new();
+    mailbox #(adder_tx) mon2scb = new();
+
+    function new(virtual adder_if vif);
+        gen = new(gen2drv);
+        drv = new(vif, gen2drv);
+        mon = new(vif, mon2scb);
+        scb = new(mon2scb);
+    endfunction
+
+    task run();
+        fork
+            gen.run();
+            drv.run();
+            mon.run();
+            scb.run();
+        join_none
+    endtask
+endclass
