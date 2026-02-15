54ecfa7b3c64808928e52fb6d8a5f71b7d36254f
Aditya Lonkar <adityalonkar99@gmail.com>
2026-02-15 13:15:52 +0530

    interface

---
+++ Adder/TB/TB_UVM/interface/adder_if.sv
@@ -0,0 +1,14 @@
+interface adder_if(input logic clk);
+
+  logic rst;
+  logic en;
+  logic [7:0] a;
+  logic [7:0] b;
+  logic [8:0] sum;
+
+  clocking cb @(posedge clk);
+    output en, a, b;
+    input  sum;
+  endclocking
+
+endinterface
